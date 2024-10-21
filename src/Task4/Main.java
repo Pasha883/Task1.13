6ce85d9a3994a1ef1fc48f0d73bd6cec81aafbed
Pasha883 <pashaboss883@gmail.com>
2024-10-21 20:45:14 +0300

    New task

---
+++ src/Task4/Main.java
@@ -0,0 +1,42 @@
+package Task4;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        Vector <String> vec = new Vector<>();
+
+        String s = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+
+            if (str.charAt(i) != ' ') {
+                s = s + str.charAt(i);
+            }
+            else{
+                vec.add(s);
+
+                s = "";
+            }
+        }
+
+        int max = 0;
+        String maxx = "";
+
+        for(String s1 : vec) {
+            if (s.length() > max) {
+                max = s1.length();
+                maxx = s1;
+            }
+        }
+
+        System.out.println(maxx);
+        System.out.println(max);
+    }
+}
